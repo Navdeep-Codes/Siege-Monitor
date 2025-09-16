38914aaf499d869f46fe2055cf0c6ea1ea07b22c
Mr-Singh-69 <navdeep13dps@gmail.com>
2025-09-16 21:19:41 +0530

    change up testing

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 const { diff } = require('deep-diff');
 
 const WEBHOOK_URL = 'https://hooks.airtable.com/workflows/v1/genericWebhook/appnIDvEOHZeznEys/wflEElnCg9y67vkUS/wtrE8ue2yIU4a8k8b';
-const DATA_URL = '';
+const DATA_URL = 'https://raw.githubusercontent.com/Navdeep-Codes/Siege-Monitor/main/store.json';
 
 async function downloadJson(url) {
     const res = await axios.get(url);
