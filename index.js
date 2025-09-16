6a11e51601a46058e2dc7db5855bb72cc323a85c
Mr-Singh-69 <navdeep13dps@gmail.com>
2025-09-16 21:27:24 +0530

    fixed

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 const { diff } = require('deep-diff');
 
 const WEBHOOK_URL = 'https://hooks.airtable.com/workflows/v1/genericWebhook/appnIDvEOHZeznEys/wflEElnCg9y67vkUS/wtrE8ue2yIU4a8k8b';
-const DATA_URL = 'https://raw.githubusercontent.com/hackclub/siege/main/config/tech_tree_data.json';
+const DATA_URL = 'https://raw.githubusercontent.com/Navdeep-Codes/Siege-Monitor/refs/heads/main/store-data.json';
 
 async function downloadJson(url) {
     try {
